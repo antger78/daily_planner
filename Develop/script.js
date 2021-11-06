ff0a334f5409cee5ec88048e8e3c462b35c5152f
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-11-06 09:21:32 +0900

    working through function

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,29 @@
+// Get date at top of screen
 const today = moment().format('LL');
 $( "#currentDay").append("Today is "+ today);
 
-var timeSlot;
+// getting current, past and future hours to highlight appropriately
+var ninthHour = $("hour-9-number").text();
+
 
 window.onload = function updateTimeSlotItems(){
-    var now = moment().hour()
-    console.log (now)
+    console.log(ninthHour);
+    if(ninthHour = moment().hour() + ":00"){
+        console.log("yes")
+    }
+    else{
+        console.log("no")
+    }
+
+    // var now = moment().hour();
+    // if ("          " + now + ":00" == currentHour){
+    // currentHour.closest().addClass("present")
+    // }
+    // else {
+    //     currentHour.closest().addClass("past")
+    //     };
 };
+
+// var ninthHour = $("hour-9").text();
+// var timeSlot;
+
