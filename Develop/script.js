1e3b9c3d341a9999b302c4a744e9ed3179f0182d
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-11-05 20:48:50 -0400

    work through highlighting cells appropriately

--- Develop/script.js
+++ Develop/script.js
@@ -5,25 +5,25 @@ $( "#currentDay").append("Today is "+ today);
 // getting current, past and future hours to highlight appropriately
 var ninthHour = $("hour-9-number").text();
 
-
 window.onload = function updateTimeSlotItems(){
     console.log(ninthHour);
+    console.log(moment().hour());
     if(ninthHour = moment().hour() + ":00"){
-        console.log("yes")
+        console.log("yes");
+        document.getElementById("hour-9-text-area").style.backgroundColor = "#ff6961";
     }
     else{
-        console.log("no")
-    }
+        document.getElementById("hour-9-text-area").style.backgroundColor = "#ffffff";
+        console.log("no");
+    };
+};
 
-    // var now = moment().hour();
+// var ninthHour = $("hour-9").text();
+// var timeSlot;
+ // var now = moment().hour();
     // if ("          " + now + ":00" == currentHour){
     // currentHour.closest().addClass("present")
     // }
     // else {
     //     currentHour.closest().addClass("past")
     //     };
-};
-
-// var ninthHour = $("hour-9").text();
-// var timeSlot;
-
